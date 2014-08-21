b7e7ce9b9a96a79c74b86e82a4d63bae24756add
Kaleb Lape <pingortle@gmail.com>
2014-08-21 15:34:42 -0500

    Add mail functionality.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
+ruby '2.1.2'
+
 source 'https://rubygems.org'
 gem 'sinatra', '1.4.5'
 gem 'haml'
 gem 'sass'
+gem 'mail'
+gem 'rest-client'
--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
+require 'mail'
+require 'rest_client'
 require 'sass/plugin/rack'
 
+require './env' if File.exists? 'env.rb'
+CAPTCHA_USERNAME = ENV['CAPTCHA_USERNAME']
+CAPTCHA_SECRET = ENV['CAPTCHA_SECRET']
+MAILGUN_KEY = ENV['MAILGUN_KEY']
+MAILGUN_SUBDOMAIN = ENV['MAILGUN_SUBDOMAIN']
+TICKETS_EMAIL_ADDRESS = ENV['TICKETS_EMAIL_ADDRESS']
+
+require './captcha'
+
 use Sass::Plugin::Rack
+
+enable :sessions
 
 configure do
   use Rack::Static,
@@ -13,5 +26,27 @@ configure do
 end
 
 get '/' do
-  haml :index, :format => :html5
+  random = rand(36**16).to_s(36)
+  session[:random] = random
+  haml :index, :format => :html5, :locals => {:random => random}
+end
+
+post '/' do
+  random = session[:random]
+  halt 403 if Captcha.get_text(CAPTCHA_SECRET, random) != params["captcha"]
+  email = params[:email]
+  name = params[:name]
+
+  html_body = haml(:email_template, :locals => {:data => params})
+
+  key = MAILGUN_KEY
+  subdomain = MAILGUN_SUBDOMAIN
+  tickets_email = TICKETS_EMAIL_ADDRESS
+  RestClient.post "https://api:#{key}@api.mailgun.net/v2/#{subdomain}/messages",
+    :from => "#{name} <#{email}>",
+    :to => tickets_email,
+    :subject => "New Ticket",
+    :html => html_body
+
+  haml :success, :format => :html5
 end
